2fc18abd1d0175685d8cce10dae7828ef0dd2527
Nanna <5608562@RTS-365.dk>
2019-04-24 12:07:25 +0200

    change function names

--- del-2/5-range-sliders/javascript-test.js
+++ del-2/5-range-sliders/javascript-test.js
@@ -6,22 +6,22 @@
 let testColorElement = document.querySelector("#testcolor");
 let slidesContainer = document.querySelector(".slidecontainer");
 let sliderInputArray = document.querySelectorAll(".slidecontainer input");
-let valueArray = [];
+let hslValueArray = [];
 
 slidesContainer.addEventListener('input', getValueFromInput);
 
 function getValueFromInput(){
     sliderInputArray.forEach(slider =>{
-        valueArray.push(slider.value);
+        hslValueArray.push(slider.value);
     })
     outputHSLValues();
 }
 
 function outputHSLValues(){
-    let hslValue = `hsl(${valueArray[0]}, ${valueArray[1]}%, ${valueArray[2]}%)`;
+    let hslValue = `hsl(${hslValueArray[0]}, ${hslValueArray[1]}%, ${hslValueArray[2]}%)`;
     console.log(hslValue);
     testColorElement.style.backgroundColor = hslValue;
-    valueArray = [];
+    hslValueArray = [];
 }
 
 

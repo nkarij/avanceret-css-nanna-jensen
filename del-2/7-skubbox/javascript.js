d84d3d83aa335d15e1e191a6aa78f97b54b31639
Nanna <5608562@RTS-365.dk>
2019-04-26 12:41:57 +0200

    8 - egen opgave

---
+++ del-2/7-skubbox/javascript.js
@@ -0,0 +1,13 @@
+/*---------------------------------------------------
+TABLE OF CONTENT 
+----------------------------------------------------*/
+// alert("hej");
+
+// Box ud til venstre
+// box skal ligge inden i en parent-html
+// position relative/absolute, skub det ud med margin minus.
+
+// badge på icon
+// badget skal ligge inden i en parent-html
+// position relative/absolute, rigth/bottom
+  
---
+++ del-2/8-progressbar/javascript.js
@@ -0,0 +1,51 @@
+/*---------------------------------------------------
+TABLE OF CONTENT 
+----------------------------------------------------*/
+document.addEventListener('DOMContentLoaded', function(){
+    // alert("hej");
+    
+    let index = 0;
+    let progressBarElement = document.querySelector("#progressbar");
+    progressBarElement.value = 0;
+
+    // en function som kører når progressbar kommer inden for synsvidde.
+
+    document.addEventListener('scroll', function(){
+        if(inViewPort(progressBarElement)){ 
+            startTimer();
+        }
+    });
+
+    function inViewPort(element){
+        // giver mig elementets koordinater/position:
+        let elementPosition = element.getBoundingClientRect();
+        // console.log(elementPosition);
+        // tjek om elementets top-position er over innerheight eller bottom er mindre end 0
+        // hvis ja er elementet er udenfor viewport, og betingelsen skal returnere false
+        if(elementPosition.top > innerHeight || elementPosition.bottom < 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // function som starter og stopper et interval
+    function startTimer(){
+        let interval = setInterval(incrementValue, 100);
+
+        function incrementValue(){
+            if(progressBarElement.value >= 0 && progressBarElement.value < 100) {
+                let addedValue = index++;
+                // setInterval(incrementValue, 100);
+                progressBarElement.value = addedValue;
+            } else {
+                console.log("færdig");
+                clearInterval(interval);
+            }
+        }
+
+    }
+    
+
+});
+
